ae0389553ce0ea2aad48e8c3231dfb0c2c9a0a08
1 <1>
2015-05-20 21:19:24 +0300

    fixed

--- Smaple/Vector3.h
+++ Smaple/Vector3.h
@@ -1,7 +1,12 @@
+#ifndef Vector3_INCLUDED
+#define Vector3_INCLUDED
 struct Vector3
 {
     float X;
     float Y;
     float Z;
+    Vector3(): X(0), Y(0), Z(0){}
     Vector3(float X, float Y, float Z) : X(X), Y(Y), Z(Z){}
+    Vector3(Vector3& other) : X(other.X), Y(other.Y), Z(other.Z){}
 };
+#endif
